5788429ff498fdb9ef0492209e9d7c15a3d5ab71
Sara <sara.diaz.oporto@gmail.com>
2025-12-05 11:45:06 -0500

    alert example

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,12 @@
-import ListGroup from "./components/ListGroup"
+import Alert from "./components/Alert"
+
 
 function App() {
-  let items = ["Arequipa", "Cusco", "Lima", "Trujillo", "Chiclayo"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
 
   return <div>
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}></ListGroup>
+    <Alert>
+      Alertaaaa<span>!!!</span>
+    </Alert>
     </div>
 }
 
